ef09b6f4acfb25f98c8b0f3371ad6dfdab545ff1
diegochagas30 <140138148+diegochagas30@users.noreply.github.com>
2023-11-05 22:59:24 -0300

    Implementação do componente de menu

--- desafio-final/src/App.jsx
+++ desafio-final/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
+import Menu from './Menu.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <div id='black'></div>
         <div id='container'>
           <Header/>
+          <Menu/>
         </div>
       </div>  
     </>
---
+++ desafio-final/src/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Menu.css'
+
+function Menu(){
+
+    return(
+        <>
+            <div id="menu-container">
+                <span class="material-icons" id="play_circle">play_circle</span>
+                <span class="material-symbols-outlined" id="favorite">favorite</span>
+                <span class="material-icons" id="arrow_circle_down">arrow_circle_down</span>
+                <span class="material-icons" id="circle">circle circle circle</span>
+            </div>
+        </>
+    )
+
+}
+
+export default Menu
